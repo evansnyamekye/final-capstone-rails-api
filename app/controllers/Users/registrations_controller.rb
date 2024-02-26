bb5e47e716151ddfaf390ce643ac9f2539635e1c
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-26 11:39:35 +0200

    Correct rubocop offenses

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -1,24 +1,22 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   private
 
-  def respond_with(resource, options={})
+  def respond_with(resource, _options = {})
     if resource.persisted?
       render json: {
         status: {
           code: 200,
           message: 'Signed up successfully',
-          data: resource 
+          data: resource
         }
       }, status: :ok
     else
       render json: {
         status: {
           message: 'User could not be created successfully',
-          error: resource.errors.full_messages,
+          error: resource.errors.full_messages
         }
       }, status: :unprocessable_entity
     end
--- app/controllers/Users/sessions_controller.rb
+++ app/controllers/Users/sessions_controller.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
   private
 
-  def respond_with(resource, options={})
-   render json: {
-    status: { code:200, message: 'User signed in successfully', data: current_user }
-   }, status: :ok
+  def respond_with(_resource, _options = {})
+    render json: {
+      status: { code: 200, message: 'User signed in successfully', data: current_user }
+    }, status: :ok
   end
 end
