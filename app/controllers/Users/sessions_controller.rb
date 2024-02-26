8829137e53157fe51a3e89093de855b0d977350f
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-26 11:30:29 +0200

    Define sessions action to login user

--- app/controllers/Users/sessions_controller.rb
+++ app/controllers/Users/sessions_controller.rb
@@ -1,27 +1,13 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def respond_with(resource, options={})
+   render json: {
+    status: { code:200, message: 'User signed in successfully', data: current_user }
+   }, status: :ok
+  end
 end
