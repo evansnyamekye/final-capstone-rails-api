cb2a1117df55718310b44a8497d83afa28afddaf
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-04 21:30:58 +0200

    Correct user spec test case rubocop offenses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let (:user) { User.create(name: 'John Doe', email: 'john.doe@example.com', password: 'password') }
+  let(:user) { User.create(name: 'John Doe', email: 'john.doe@example.com', password: 'password') }
 
   it 'is valid with valid attributes' do
     expect(user).to be_valid
